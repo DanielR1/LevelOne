a3d2a60e9b8e85688da30bfe765976d7a5c3d4c9
DanielR1 <league@Mac-mini-2.local>
2016-06-29 19:02:01 -0700

    3
    /\

---
+++ src/Dog.java
@@ -0,0 +1,28 @@
+
+public class Dog {
+	String type;
+	String size;
+	int weight;
+	String gender;
+Dog(String type, String size, int weight, String gender){
+	this.type = type;
+	this.size = size;
+			this.weight = weight;
+			this.gender = gender;
+}
+void bark(){
+	System.out.println("Woof Woof");
+	
+}
+void eat(){
+	System.out.println("This is delicious");
+	
+}
+void run(){
+	System.out.println("im running!");
+	
+}
+void roll(){
+	System.out.println("pls scratch my belly");
+}
+}
---
+++ src/DogRunner.java
@@ -0,0 +1,8 @@
+
+public class DogRunner {
+public static void main(String[] args) {
+	Dog jimmy = new Dog("Golden Retriever", "Small", 60, "Male");
+	jimmy.bark();
+	jimmy.eat();
+}
+}
